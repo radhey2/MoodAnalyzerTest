20a903e6a4ca716d7a2376524f2eb967aa19427a
radhey2 <96431541+radhey2@users.noreply.github.com>
2022-03-30 11:49:03 +0530

    Added : Refactor

--- src/main/java/MoodAnalyzer.java
+++ src/main/java/MoodAnalyzer.java
@@ -1,7 +1,11 @@
 public class MoodAnalyzer {
     String message;
 
-    public String analyseMood(String message) {
+    public MoodAnalyzer(String message) {
+        this.message = message;
+    }
+
+    public String analyseMood() {
 
         if (message.contains("Sad")) {
             System.out.println(message);
