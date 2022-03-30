97a55e3a2c3fd562ec891d7caba440d382b05837
radhey2 <96431541+radhey2@users.noreply.github.com>
2022-03-30 11:47:27 +0530

    Added : Refactor

--- src/test/java/MoodAnalyzerTest.java
+++ src/test/java/MoodAnalyzerTest.java
@@ -4,15 +4,15 @@ import org.junit.jupiter.api.Test;
 public class MoodAnalyzerTest {
     @Test
     public void givenMessage_whenSad_ShouldReturnSad() {
-        MoodAnalyzer moodAnalyser = new MoodAnalyzer();
-        String mood = moodAnalyser.analyseMood("this is a Sad Message");
+        MoodAnalyzer moodAnalyser = new MoodAnalyzer("this is a Sad Message");
+        String mood = moodAnalyser.analyseMood();
         Assertions.assertEquals("SAD", mood);
         System.out.println(mood);
     }
     @Test
     public void givenMessage_whenHappy_ShouldReturnHappy(){
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-        String mood1 = moodAnalyzer.analyseMood("this is a Happy Message");
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("this is a Happy Message");
+        String mood1 = moodAnalyzer.analyseMood();
         Assertions.assertEquals("HAPPY",mood1);
         System.out.println(mood1);
     }
